c3c99025eaf8cace92883c69a90718db88d98997
Beattle <ea29db6e18>
2017-04-19 15:22:17 +0300

    sync

---
+++ bts/test/.left.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Деревообрабатывающее оборудование",
+		"/catalogue/derevoobrabatyvayushchee_oborudovanie/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Металлобрабатывающее оборудование",
+		"/catalogue/metalloobrabatyvayushchee_oborudovanie/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Стеклобрабатывающее оборудование",
+		"/catalogue/stekloobrabatyvayushchee_oborudovanie/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Б/у оборудование",
+		"/catalogue_bu/",
+		Array(),
+		Array(),
+		""
+	)
+);
+?>
